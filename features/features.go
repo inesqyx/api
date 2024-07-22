f0b054570edf8dda6831281838936d48161349ac
Ines Qian <iqian@redhat.com>
2024-07-22 16:33:41 -0400

    Added Feature Gate for Bootc Update Path

--- features/features.go
+++ features/features.go
@@ -348,6 +348,13 @@ var (
 					enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 					mustRegister()
 
+	FeatureGateBootcUpdatePath = newFeatureGate("BootcUpdatePath").
+					reportProblemsToJiraComponent("MachineConfigOperator").
+					contactPerson("inesqyx").
+					productScope(ocpSpecific).
+					enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+					mustRegister()
+
 	FeatureGateSignatureStores = newFeatureGate("SignatureStores").
 					reportProblemsToJiraComponent("Cluster Version Operator").
 					contactPerson("lmohanty").
